5e6696b35710086e7d263f6c3d3ef8eb3fa67606
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2023-08-23 23:59:34 +0600

    Category Controller And Views compleate

--- EcommerceWeb/Controllers/CategoryController.cs
+++ EcommerceWeb/Controllers/CategoryController.cs
@@ -37,5 +37,64 @@ namespace EcommerceWeb.Controllers
             }
             return View(category);
         }
+
+        //Edit Get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        //Edit Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        //Delete get
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        //Delete Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                /*TempData["success"] = "Category Delete Successfully";*/
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
     }
 }
