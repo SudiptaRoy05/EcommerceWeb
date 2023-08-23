29bb3c2f94fe5e2fadba1cccf896605fabc1f682
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2023-08-23 23:37:08 +0600

    More Update

---
+++ EcommerceWeb/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using EcommerceWeb.Data;
+using EcommerceWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Categories;
+            return View(categories);
+        }
+
+        //Create Get 
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Create Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}
