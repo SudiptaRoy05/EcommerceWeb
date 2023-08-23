835b0b2eab32152eda6a65b09719f708782f8317
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2023-08-23 23:11:41 +0600

    Next Push

---
+++ EcommerceWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public int DiaplayOrder { get; set; }
+
+        /*public DateTime CreatedDateTime { get; set; } = DateTime.Now;*/
+
+    }
+}
